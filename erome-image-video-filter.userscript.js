41f9bddfcee9291c97ab9c8b8308af764d6b4bf1
heinrichdede <122832996+heinrichdede@users.noreply.github.com>
2023-11-03 03:25:07 +0200

    Add download URL and bump version to 0.2.0

--- erome-image-video-filter.userscript.js
+++ erome-image-video-filter.userscript.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Erome Image/Video Filter
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2.0
 // @description  Hide or show images and Videos
+// @downloadURL  https://github.com/heinrichdede/erome-image-video-filter/raw/master/erome-image-video-filter.userscript.js
 // @author       heinrichdede
 // @match        https://www.erome.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
