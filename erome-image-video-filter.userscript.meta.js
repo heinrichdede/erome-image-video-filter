f23a28a83e80453a153089c272e4b22fb4b5db3e
heinrichdede <122832996+heinrichdede@users.noreply.github.com>
2023-11-03 03:30:31 +0200

    Create erome-image-video-filter.userscript.meta.js file

---
+++ erome-image-video-filter.userscript.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Erome Image/Video Filter
+// @namespace    http://tampermonkey.net/
+// @version      0.2.0
+// @description  Hide or show images and Videos
+// @updateURL    https://raw.githubusercontent.com/heinrichdede/erome-image-video-filter/main/erome-image-video-filter.userscript.meta.js
+// @downloadURL  https://raw.githubusercontent.com/heinrichdede/erome-image-video-filter/main/erome-image-video-filter.userscript.js
+// @author       heinrichdede
+// @match        https://www.erome.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM_addStyle
+// ==/UserScript==
