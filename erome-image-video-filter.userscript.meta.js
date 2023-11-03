84ab93ca3532ae0110b272bd9d57dc86c32f7b05
heinrichdede <122832996+heinrichdede@users.noreply.github.com>
2023-11-03 03:37:10 +0200

    Update erome-image-video-filter.userscript.meta.js with updated meta

--- erome-image-video-filter.userscript.meta.js
+++ erome-image-video-filter.userscript.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Erome Image/Video Filter
 // @namespace    http://tampermonkey.net/
-// @version      0.2.0
+// @version      0.2.1
 // @description  Hide or show images and Videos
-// @updateURL    https://raw.githubusercontent.com/heinrichdede/erome-image-video-filter/main/erome-image-video-filter.userscript.meta.js
-// @downloadURL  https://raw.githubusercontent.com/heinrichdede/erome-image-video-filter/main/erome-image-video-filter.userscript.js
+// @updateURL    https://github.com/heinrichdede/erome-image-video-filter/raw/main/erome-image-video-filter.userscript.meta.js
+// @downloadURL  https://github.com/heinrichdede/erome-image-video-filter/raw/main/erome-image-video-filter.userscript.js
 // @author       heinrichdede
 // @match        https://www.erome.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
